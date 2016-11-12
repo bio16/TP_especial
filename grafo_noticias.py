7a58142695baa6704d576cc62ef9fbfc59471d91
jimsrc <jimmy.ilws@gmail.com>
2016-11-12 12:36:54 -0300

    DRL seems to be better than the others (too slow to run)

--- grafo_noticias.py
+++ grafo_noticias.py
@@ -8,7 +8,37 @@ corr_matrix = np.load('corr_matrix100.npy')
 graph = igraph.Graph.Weighted_Adjacency(list(corr_matrix), mode = igraph.ADJ_MAX, loops = False)
 
 # Devuelvo valores del grafo
-print 'Numero de nodos: ', len(graph.vs)
-print 'Numero de enlaces: ', len(graph.es)
-print 'Grafo dirigido? ', graph.is_directed()
-print 'Grafo Pesado? ', graph.is_weighted()
+print('Numero de nodos: ', len(graph.vs))
+print('Numero de enlaces: ', len(graph.es))
+print('Grafo dirigido? ', graph.is_directed())
+print('Grafo Pesado? ', graph.is_weighted())
+
+opt = {
+    'vertex_size' : 5,
+    'edge_width'  : 0.5,
+    'opacity' : 0.3,
+}
+
+"""
+# Grafo Fruchterman - Reingold
+layout = graph.layout_fruchterman_reingold()
+igraph.plot(graph, layout = layout, target = 'FrutRein.png')
+
+# Grafo Kamada Kawai
+layout = graph.layout_kamada_kawai()
+igraph.plot(graph, layout = layout, target = 'KamKaw.png')
+"""
+# Grafo DrL
+layout = graph.layout_drl()
+igraph.plot(graph, layout = layout, target = 'Drl.png', **opt)
+
+# Layouts que NO nos da una buena visualización
+
+## Grafo Random
+#layout = graph.layout_random()
+#igraph.plot(graph, layout = layout, target = 'Random.png')
+#
+## Grafo Multidimensional Scaling
+#layout = graph.layout_mds()
+#igraph.plot(graph, layout = layout, target = 'Multi.png')
+#EOF
