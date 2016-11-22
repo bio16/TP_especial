d2d83f23522237727fc0b3a0e97f3f95bce66091
spinto <spinto@df.uba.ar>
2016-11-22 18:32:29 -0300

    Array con la membresia de infomap, no pude actualizar el gml, maldito igraph...

--- Seba_red_buena/read_graph.py
+++ Seba_red_buena/read_graph.py
@@ -1,19 +1,14 @@
 import igraph
 import random
+import numpy as np
+
 
 # Cargo en el objeto graph la red de dolphins.gml.
-graph = igraph.read('red_060.gml')
-
+graph = igraph.read('news-weighted_0.60-corr_with-dates_with-comms.gml')
 random.seed(123457)
 
 com = graph.community_infomap()
 membership = com.membership
 
-print graph.modularity(membership)
+np.save('infomap_membership.npy', membership)
 
-for i in range(len(graph.vs)):
-    graph.vs['infomap_membership'] = membership[i]
-
-fp = file('red_con_infomap.gml','a')
-graph.write_gml(fp)
-fp.close()
