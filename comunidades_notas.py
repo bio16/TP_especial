5127ee4a48dc0a508d135905fb305a04ceceb2a5
spinto <spinto@df.uba.ar>
2016-11-25 18:23:54 -0300

    Nada...algunos cambios

--- comunidades_notas.py
+++ comunidades_notas.py
@@ -9,19 +9,15 @@ news = Newspaper('data/LaNacion.xml')
 
 info_communities = pk.load(file('Communities_infomap.pk','r'))
 
-#fp = codecs.open('Comunities_titulos.txt','w','utf8')
-#fp.write('#id (original network) - Community infomap \n')
 
-for i in range(len(info_communities)):
-#    fp.write(str(i) + '\n')
-    for j in info_communities[i]:
-        if int(j) == 176:
-            title = news.getNotebyId(idNotes[int(j)]).title
-            body = news.getNotebyId(idNotes[int(j)]).body
-            print title
-            print body
-            exit()
-#        fp.write(str(int(j)) + ' ' + title + '\n')
-#    fp.write('\n')
+def extract_notes(list_nodes):
 
-fp.close()
+    for i in range(len(info_communities)):
+        for j in info_communities[i]:
+            if int(j) in list_nodes:
+                title = news.getNotebyId(idNotes[int(j)]).title
+                body = news.getNotebyId(idNotes[int(j)]).body
+                print title
+                print body[:200]
+            
+
