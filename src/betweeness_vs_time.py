91eeaabdb1a2ab89595065868d9515459a31f532
jimsrc <jimmy.ilws@gmail.com>
2016-11-29 02:25:49 -0300

    <betweenness> vs time, with error band

--- src/betweeness_vs_time.py
+++ src/betweeness_vs_time.py
@@ -4,6 +4,8 @@ import igraph
 from datetime import datetime, timedelta
 import numpy as np
 from pylab import plot, figure, close, show, grid
+import matplotlib.patches as patches
+import matplotlib.transforms as transforms
 
 fname = '../jimmy.gml'
 g = igraph.read(fname)
@@ -34,25 +36,34 @@ nd2 = (nd-1)/2
 clus = np.array(g.vs['clustering'])
 # nmbr of days where I'll analyze
 ndates = dates[nd2:-nd2].size 
-_clus = np.zeros(ndates, dtype=np.float32)
-_clus_std = np.zeros(ndates, dtype=np.float32)
+t_days = np.arange(nd2+1, ndates+nd2+1, 1) #----------------
+_bet = np.zeros(ndates, dtype=np.float32)
+_bet_std = np.zeros(ndates, dtype=np.float32)
 for date, i in zip(dates[nd2:-nd2], range(ndates)):
     cc  = _dates>=(date-timedelta(days=nd2))
     cc &= _dates<=(date+timedelta(days=nd2))
-    _clus[i] = np.array(g.vs['clustering'])[cc].mean()
-    _clus_std[i] = np.array(g.vs['clustering'])[cc].std()/(1.*nd)
+    # nodes in this time window
+    nodes = cc.nonzero()[0]
+    _bet[i] = np.array(g.betweenness())[nodes].mean()
+    _bet_std[i] = np.array(g.betweenness())[nodes].std()
+    print(i,ndates)
 
-t_days = np.arange(nd2+1, ndates+nd2+1, 1)
 
 #---
 fig = figure(1, figsize=(6,4))
 ax  = fig.add_subplot(111)
-ax.plot(t_days, _clus, '-o')
+ax.plot(t_days, _bet, '-ok')
 
-ax.set_xlabel('nro de dias desde %s'%dates[0].strftime('%d/%B/Y'))
-ax.set_ylabel('<clustering>')
-ax.grid()
-fig.savefig('./clus_vs_time.png', dpi=135, bbox_inches='tight')
+# banda de error
+inf     = _bet + _bet_std/np.sqrt(nd)
+sup     = _bet - _bet_std/np.sqrt(nd)
+ax.fill_between(t_days, inf, sup, facecolor='gray', alpha=0.5)
+
+ax.set_xlabel('nro de dias desde %s'%dates[0].strftime('%d/%B/%Y'))
+ax.set_ylabel('<betweenness>')
+ax.set_ylim(0.,)
+ax.grid(True)
+fig.savefig('./betweenness_vs_time.png', dpi=135, bbox_inches='tight')
 close(fig)
 
 #EOF
